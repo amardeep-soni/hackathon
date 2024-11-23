31b7a68d73822aeaee6bf376ae9711038dc5a776
shyamkryadav <shyamkryadav345@gmail.com>
2024-11-23 20:12:58 +0545

    gsap added and slider updated

--- WebApp/src/app/pages/camp-details/camp-details.component.ts
+++ WebApp/src/app/pages/camp-details/camp-details.component.ts
@@ -5,9 +5,9 @@ import {
   CampsDto,
   CampsServiceProxy,
 } from '../../../shared/service-proxies/service-proxies';
-
+import { gsap } from 'gsap';
 @Component({
-  selector: 'app-camp-details',
+  selector: 'app-camp-details', 
   standalone: true,
   imports: [],
   templateUrl: './camp-details.component.html',
@@ -25,44 +25,38 @@ export class CampDetailsComponent {
     //   this.camp = data;
     //   console.log(data);
     // });
+    // this.gsapAnimation();
     this.camp = new CampsDto({
-      campName: "Adventure Summer Camp",
-      address: "123 Forest Lane, Springfield, USA",
-      email: "info@adventuresummercamp.com",
-      phone: "123-456-7890",
-      activitiesOffered: [
-          "Hiking",
-          "Kayaking",
-          "Archery",
-          "Rock Climbing"
-      ],
-      datesAndDurations: [
-          "June 1 - June 15",
-          "July 1 - July 15"
-      ],
-      ageGroup: "8-14 years",
-      costsAndScholarships: "Standard: $500/week, Scholarships Available",
+      campName: 'Adventure Summer Camp',
+      address: '123 Forest Lane, Springfield, USA',
+      email: 'info@adventuresummercamp.com',
+      phone: '123-456-7890',
+      activitiesOffered: ['Hiking', 'Kayaking', 'Archery', 'Rock Climbing'],
+      datesAndDurations: ['June 1 - June 15', 'July 1 - July 15'],
+      ageGroup: '8-14 years',
+      costsAndScholarships: 'Standard: $500/week, Scholarships Available',
       testimonialsOrReviews: [
-          "Best camp ever! - John D.",
-          "My kids loved it. - Sarah L."
+        'Best camp ever! - John D.',
+        'My kids loved it. - Sarah L.',
       ],
-      classSchedule: [
-          "Morning Yoga",
-          "Team Activities",
-          "Evening Campfire"
-      ],
-      gender: "Co-ed",//
-      price: "$500 per week",//
-      startDate: "2024-06-01",
-      endDate: "2024-07-15",
-      capacity: "50 campers",
-      registrationDeadline: "2024-05-15",
-      spotsAvailable: "20",
-      highlights: "Outdoor Adventures, Experienced Staff, Lifelong Memories",
-      language: "English",
-      category: "Outdoor Adventure",
-      campLink: "http://idtech.com",
-      imageLink: "https://s3.us-west-1.amazonaws.com/idtech-2018-media-prd/header_carousel2_rblx2_b_31b275ed2d.jpg",
-      hostedBy: "amar"});
+      classSchedule: ['Morning Yoga', 'Team Activities', 'Evening Campfire'],
+      gender: 'Co-ed', //
+      price: '$500 per week', //
+      startDate: '2024-06-01',
+      endDate: '2024-07-15',
+      capacity: '50 campers',
+      registrationDeadline: '2024-05-15',
+      spotsAvailable: '20',
+      highlights: 'Outdoor Adventures, Experienced Staff, Lifelong Memories',
+      language: 'English',
+      category: 'Outdoor Adventure',
+      campLink: 'http://idtech.com',
+      imageLink:
+        'https://s3.us-west-1.amazonaws.com/idtech-2018-media-prd/header_carousel2_rblx2_b_31b275ed2d.jpg',
+      hostedBy: 'amar',
+    });
+  }
+  gsapAnimation() {
+    gsap.from('.contactInfo', { duration: 1.5, x: 200, opacity: 0 });
   }
 }

8813df2cb32b8cec129cacfd1d9d12398d322e71
shyamkryadav <shyamkryadav345@gmail.com>
2024-11-23 13:06:06 +0545

    hero section and header created

---
+++ WebApp/src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule,RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isDarkMode = false;
+  isMenuOpen = false;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const body = document.body;
+    body.classList.toggle('dark-mode', this.isDarkMode);
+    body.classList.toggle('light-mode', !this.isDarkMode);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
